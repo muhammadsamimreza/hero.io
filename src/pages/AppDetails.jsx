76355e6b2151bf90a27af98b623ad66c845aedce
muhammad samim reza <muhammadsamimreza@gmail.com>
2025-10-08 12:26:51 +0600

    App details functionality done when click on App card

--- src/pages/AppDetails.jsx
+++ src/pages/AppDetails.jsx
@@ -11,43 +11,48 @@ const AppDetails = () => {
   const { appData } = useAppData();
   const appId = Number(id);
 
-  const data = appData.find((data) => data.id === appId);
+  const data = appData.find((data) => data.id === appId) || {};
 
-  console.log(data);
+  const {title, image, ratingAvg,downloads,reviews} = data;
+ 
+  
+  
 
   return (
     <div>
       <Container>
-        <div className="flex items-center gap-20">
-          <div className="w-44 h-60 bg-gray-300 rounded-2xl">img</div>
+        <div className="flex items-center gap-20 my-5 p-3 bg-base-200 rounded-2xl">
+          <div className="w-44 h-60 bg-gray-300 rounded-2xl">
+            <img className="w-full h-full object-cover" src={image} alt="" />
+          </div>
           <div className="space-y-3">
-            <div className="space-y-2">
-              <h1 className="text-xl font-bold">This is Home Planner</h1>
-              <p>
+            <div className="space-y-2 border-b border-gray-300 pb-5">
+              <h1 className="text-3xl font-bold">{title}</h1>
+              <p className="text-xl ">
                 <span className="text-gray-400">Develop by:</span>
-                <span>Epic Planner</span>
+                <span className="font-medium bg-[linear-gradient(125.07deg,_rgba(99,46,227,1),_rgba(159,98,242,1)_100%)] text-transparent bg-clip-text"> Epic Planner</span>
               </p>
             </div>
-            <div className="flex items-center gap-10">
+            <div className="flex items-center gap-12">
               <div className="flex flex-col">
-                <img className="w-8" src={downlowdIcon} alt="" />
+                <img className="w-7" src={downlowdIcon} alt="" />
                 <div iv className="my-2">
-                  <p className="text-[#001931]">Download</p>
-                  <h1 className="text-3xl font-bold">8M</h1>
+                  <p className="text-[#7f8488] text-sm">Download</p>
+                  <h1 className="text-[27px] font-bold">{downloads}</h1>
                 </div>
               </div>
               <div className="flex flex-col">
-                <img className="w-8" src={ratingsIcon} alt="" />
+                <img className="w-7" src={ratingsIcon} alt="" />
                 <div iv className="my-2">
-                  <p className="text-[#001931]">Average Ratings</p>
-                  <h1 className="text-3xl font-bold">4.9</h1>
+                  <p className="text-[#7f8488] text-sm">Average Ratings</p>
+                  <h1 className="text-[27px] font-bold">{ratingAvg}</h1>
                 </div>
               </div>
               <div className="flex flex-col">
-                <img className="w-8" src={reviewIcon} alt="" />
+                <img className="w-7" src={reviewIcon} alt="" />
                 <div iv className="my-2">
-                  <p className="text-[#001931]">Total Review</p>
-                  <h1 className="text-3xl font-bold">57K</h1>
+                  <p className="text-[#7f8488] text-sm">Total Review</p>
+                  <h1 className="text-[27px] font-bold">{reviews}</h1>
                 </div>
               </div>
             </div>
