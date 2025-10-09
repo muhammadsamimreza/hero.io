5853f967c2026629012af7b545225be1092289f6
muhammad samim reza <muhammadsamimreza@gmail.com>
2025-10-09 10:26:02 +0600

    space fixed installed page

--- src/pages/Instalation.jsx
+++ src/pages/Instalation.jsx
@@ -18,23 +18,27 @@ const Instalation = () => {
       savedAppLocal.map(Number).includes(app.id)
     );
 
-    setInstalledApp(filteredApp); 
+    setInstalledApp(filteredApp);
   }, [appData]);
 
   const handleSort = (type) => {
     setSort(type);
     let sortedApp = [...installedApp];
 
-   const parseDownloads = (str) => {
-    if (str.endsWith("M")) return parseFloat(str) * 1_000_000;
-    return Number(str);
-  };
+    const parseDownloads = (str) => {
+      if (str.endsWith("M")) return parseFloat(str) * 1_000_000;
+      return Number(str);
+    };
 
-  if (type === "Low-High") {
-    sortedApp.sort((a, b) => parseDownloads(a.downloads) - parseDownloads(b.downloads));
-  } else if (type === "High-Low") {
-    sortedApp.sort((a, b) => parseDownloads(b.downloads) - parseDownloads(a.downloads));
-  }
+    if (type === "Low-High") {
+      sortedApp.sort(
+        (a, b) => parseDownloads(a.downloads) - parseDownloads(b.downloads)
+      );
+    } else if (type === "High-Low") {
+      sortedApp.sort(
+        (a, b) => parseDownloads(b.downloads) - parseDownloads(a.downloads)
+      );
+    }
     setInstalledApp(sortedApp);
   };
   const handleUninstallApp = (id) => {
@@ -43,44 +47,48 @@ const Instalation = () => {
   return (
     <div className="bg-[#e9e9e9] min-h-screen">
       <Container>
-        <div className="py-10 space-y-2">
-          <h1 className="text-3xl font-bold text-center">All Instaled Apps</h1>
-          <p className="text-center">
-            Explore All Trending Apps on the Market developed by us
-          </p>
-        </div>
-        <div>
-          <div className="my-8 px-3 flex justify-between items-center">
-            <div className="text-xl font-semibold">
-              <p>({installedApp.length}) App Founded</p>
-            </div>
-            <div>
-              <details className="dropdown">
-                <summary className="btn m-1 w-40">sort by: {sort} </summary>
-                <ul className="menu dropdown-content bg-base-100 rounded-box z-1 space-y-3 w-42 p-2 shadow-sm">
-                  <li
-                    onClick={() => handleSort("High-Low")}
-                    className="hover:cursor-pointer"
-                  >
-                    High-Low
-                  </li>
-                  <li
-                    onClick={() => handleSort("Low-High")}
-                    className="hover:cursor-pointer"
-                  >
-                    Low-High
-                  </li>
-                </ul>
-              </details>
+        <div className="py-10">
+          <div className="py-5 space-y-2">
+            <h1 className="text-3xl font-bold text-center">
+              All Instaled Apps
+            </h1>
+            <p className="text-center">
+              Explore All Trending Apps on the Market developed by us
+            </p>
+          </div>
+          <div>
+            <div className="my-8 px-3 flex justify-between items-center">
+              <div className="text-xl font-semibold">
+                <p>({installedApp.length}) App Founded</p>
+              </div>
+              <div>
+                <details className="dropdown">
+                  <summary className="btn m-1 w-40">sort by: {sort} </summary>
+                  <ul className="menu dropdown-content bg-base-100 rounded-box z-1 space-y-3 w-42 p-2 shadow-sm">
+                    <li
+                      onClick={() => handleSort("High-Low")}
+                      className="hover:cursor-pointer"
+                    >
+                      High-Low
+                    </li>
+                    <li
+                      onClick={() => handleSort("Low-High")}
+                      className="hover:cursor-pointer"
+                    >
+                      Low-High
+                    </li>
+                  </ul>
+                </details>
+              </div>
             </div>
           </div>
         </div>
         <div>
           {installedApp.map((app) => (
-            <InstalledCard 
-            key={app.id} 
-            app={app}
-            onUninstall={handleUninstallApp}
+            <InstalledCard
+              key={app.id}
+              app={app}
+              onUninstall={handleUninstallApp}
             ></InstalledCard>
           ))}
         </div>
