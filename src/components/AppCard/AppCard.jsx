27f99ff50619d0e3d93db7eb4a33ad4fdc8a0a19
muhammad samim reza <muhammadsamimreza@gmail.com>
2025-10-09 11:06:29 +0600

    Hover effect on card

--- src/components/AppCard/AppCard.jsx
+++ src/components/AppCard/AppCard.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router";
+import downloadImg from "../../assets/icon-downloads.png"
+import ratingsImg from "../../assets/icon-ratings.png"
 
 
 const AppCard = ({app}) => {
   const { title,ratingAvg,downloads, image,id } = app;
   return (
     <Link to={`/appdetails/${id}`}>
-      <div className="w-60 h-72 bg-white border rounded-lg p-3">
+      <div className="w-60 h-72 bg-white border rounded-lg p-3 hover:scale-105 transition transform">
         <div className="bg-gray-400 w-full h-[70%] overflow-hidden rounded-lg">
           <img src={image} className="w-full h-full object-cover overflow-hidden"  alt="" />
         </div>
@@ -15,8 +17,11 @@ const AppCard = ({app}) => {
             <h1 className="text-xl font-semibold">{title}</h1>
           </div>
           <div className="flex justify-between">
-            <p>{downloads}</p>
-            <p>{ratingAvg}</p>
+            <p className="flex gap-2 items-center bg-green-50 px-3 py-1 font-bold rounded-xl text-green-600">
+              <span><img className="w-4" src={downloadImg} alt="" /></span> {downloads}</p>
+            <p className="flex gap-2 items-center bg-amber-50 px-3 py-1 font-bold rounded-xl text-amber-500">
+              <span><img className="w-4" src={ratingsImg} alt="" /></span> {ratingAvg}</p>
+            
           </div>
         </div>
       </div>
--- src/pages/TrendingApps.jsx
+++ src/pages/TrendingApps.jsx
@@ -6,29 +6,32 @@ import { Link } from "react-router";
 
 const TrendingApps = () => {
   const { appData } = useAppData();
-  const trendData = appData.slice(0,8);
- 
+  const trendData = appData.slice(0, 8);
 
   return (
     <div>
-      
-        <div className="">
-          <div className="text-center space-y-3 py-10">
-            <h1 className="text-3xl font-bold ">TrendingApps</h1>
-            <p className="text-[#627382]">
-              Explore All Trending Apps on the Market developed by us
-            </p>
-          </div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 justify-items-center">
-                {
-                    trendData.map(app=> <AppCard key={app.id} app={app}></AppCard>)
-                }
-          </div>
-          <div className="py-10 flex justify-center">
-             <Link to="/allapp" className="btn"> See All</Link>
-          </div>
+      <div className="">
+        <div className="text-center space-y-3 py-10">
+          <h1 className="text-3xl font-bold ">TrendingApps</h1>
+          <p className="text-[#627382]">
+            Explore All Trending Apps on the Market developed by us
+          </p>
         </div>
-    
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 justify-items-center">
+          {trendData.map((app) => (
+            <AppCard key={app.id} app={app}></AppCard>
+          ))}
+        </div>
+        <div className="py-10 flex justify-center">
+          <Link
+            to="/allapp"
+            className="btn w-48 bg-[linear-gradient(125.07deg,_rgba(99,46,227,1),_rgba(159,98,242,1)_100%)] text-white hover:scale-102 transition transform"
+          >
+            {" "}
+            See All
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
